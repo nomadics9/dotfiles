8b8ad36d04e92add14c2e20ba6205f752ed400bc
nomadics9 <binaskar9@gmail.com>
2024-10-12 17:23:41 +0300

    MD preview plugin

--- nvim/lua/kickstart/plugins/markdown-preview.lua
+++ nvim/lua/kickstart/plugins/markdown-preview.lua
@@ -1,9 +1,8 @@
 return {
   'iamcco/markdown-preview.nvim',
-  build = 'cd app && bun install',
-  config = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
-  ft = { 'markdown' },
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
 }
